c921942e5350d79b695b237f227039430946789c
uperbilite <uperbilite@gmail.com>
2022-10-13 15:55:46 +0800

    fill task 4 function

--- src/philphix.c
+++ src/philphix.c
@@ -78,6 +78,8 @@ void readDictionary(char *dictName) {
     char *key;
     char *data;
     while (1) {
+        // TODO: Read key that only contains alphanumeric characters
+        // TODO: key and data string's length can be over than 60
         key = (char *) malloc(sizeof(char) * 61);
         data = (char *) malloc(sizeof(char) * 61);
         if (fscanf(fp, "%s%s", key, data) == EOF) {
@@ -92,7 +94,79 @@ void readDictionary(char *dictName) {
 }
 
 /* Task 4 */
+int isAlphanumeric(int ch) {
+    return isalpha(ch) || (ch >= 0 && ch <= 9);
+}
+
+/*
+ * return the word starting from ch, word reading may be terminated by a
+ * non-alphanumeric character, the parameter ch will get this and return
+ */
+char *readWordFrom(int *ch) {
+    int len = 20;
+    char *word = malloc(sizeof(char) * len);
+    word[0] = (char) *ch;
+    word[1] = '\0';
+
+    while ((*ch = getchar()) != EOF && isAlphanumeric(*ch)) {
+        unsigned int s = strlen(word);
+        if (s > len * 0.75) {
+            len <<= 1;
+            word = realloc(word, sizeof(char) * len);
+        }
+        word[s] = (char) *ch;
+        word[s + 1] = '\0';
+    }
+
+    return word;
+}
+
+char *getDataFrom(char *word) {
+    char *data;
+
+    // the exact word.
+    if ((data = findData(dictionary, word)) != NULL) {
+        return data;
+    }
+
+    // the word except the first character to lower case.
+    size_t len = strlen(word);
+    for (int i = 0; i < len; i++) {
+        word[i] = (char) tolower(word[i]);
+    }
+    word[0] = (char) toupper(word[0]);
+    if ((data = findData(dictionary, word)) != NULL) {
+        return data;
+    }
+
+    // the word all lower case.
+    word[0] = (char) tolower(word[0]);
+    if ((data = findData(dictionary, word)) != NULL) {
+        return data;
+    }
+
+    return word;
+}
+
 void processInput() {
-    // -- TODO --
-    fprintf(stderr, "You need to implement processInput\n");
+    int *ch;
+    ch = malloc(sizeof(int));
+
+    while ((*ch = getchar()) != EOF) {
+        if (isAlphanumeric(*ch)) {
+            char *word = readWordFrom(ch);
+            char *data = getDataFrom(word);
+            printf("%s", data);
+
+            if (isAlphanumeric(*ch)) {
+                printf("%c", *ch);
+            }
+
+            free(word);
+        } else {
+            printf("%c", *ch);
+        }
+    }
+
+    free(ch);
 }
